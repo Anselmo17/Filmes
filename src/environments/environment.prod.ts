f79c8b5ad3173e9c5760f21dd62851589193734e
Anselmo <ansinftec17@gmail.com>
2023-02-13 11:15:00 -0300

    FEAT:Setting environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  POPULAR_MOVIES: '',
+  DETAILS_MOVIE: '',
+  KEY: '',
+  LANGUAGE: 'pt-BR',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  POPULAR_MOVIES: 'https://api.themoviedb.org/3/movie/popular?',
-  DETAILS_MOVIE: 'https://api.themoviedb.org/3/movie',
-  KEY: 'd9d5478448f569ffcf97e5db8c9fdee8',
-  LANGUAGE: 'pt-BR'
+  POPULAR_MOVIES: '',
+  DETAILS_MOVIE: '',
+  KEY: '',
+  LANGUAGE: 'pt-BR',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-
