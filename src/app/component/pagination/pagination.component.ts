26a1707de41eea9333749e1982272cb6311f4280
Anselmo <ansinftec17@gmail.com>
2023-02-15 10:20:19 -0300

    FEAT:Adding styling to pagination

--- src/app/component/pagination/pagination.component.ts
+++ src/app/component/pagination/pagination.component.ts
@@ -9,11 +9,15 @@ export class PaginationComponent implements OnInit {
 
 
   @Input() movies: any;
+  pages: any;
+  limit: number = 20;
+  pagesTotal: number = 0;
 
-  
   constructor() { }
 
   ngOnInit(): void {
+    this.pagesTotal = this.movies.length / this.limit || 0;
+    this.pages = new Array(this.pagesTotal);
   }
 
 }
